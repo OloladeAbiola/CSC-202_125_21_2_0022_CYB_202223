8ec3d6d6e23a6e278310c32c7d9b9d961ddee433
OloladeAbiola <131535381+OloladeAbiola@users.noreply.github.com>
2023-06-21 21:58:39 +0100

    Day_1

---
+++ Day_1.py
@@ -0,0 +1,43 @@
+print("Hello world!")
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+# printing on different lines using backslash and n character \n
+print("Hello world!\nHello world!\nHello world!")
+# strings concatenation
+print("Hello" + "Victory")
+print("Hello" + " " + "Victory")
+# input function
+input("what is your name?")
+print("Hello" + " " + input ("what is your name? ") + "!")
+# printing numbers of character
+len(input("what is your name? "))
+# variable
+name = input(" what is your name?")
+length = len(name)
+print(length)
+# switching of values
+a = input("a:")
+b = input("b:")
+c = a
+a = b
+b = c
+print("a = " + a)
+print("b = " + b)
+# Create a greeting for your program.
+
+print("Band Name Generator")
+
+# Ask the user for the city that they grew up in.
+
+City = input("Which city do you grew up in?\n")
+
+# Ask the user the name of a pet.
+
+Pet = input("Mention the name of a pet\n")
+
+# Combine the name of their city and show them their band name
+
+print("Your band name could be " + City + " " + Pet)
+
+# Make sure that the input cursor shows on a new line
